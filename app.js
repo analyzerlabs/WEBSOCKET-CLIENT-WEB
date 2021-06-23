32cc7c5bdfbf60765642eae15bba10823f063fe8
MiguelQuispe <fisicomiguel@gmail.com>
2021-06-23 08:17:03 -0500

    p

--- app.js
+++ app.js
@@ -1,36 +1,23 @@
-const { Socket } = require('dgram');
 const express   = require('express');
 const path  = require('path');
 const app = express();
 
-
-const SocketIO = require('socket.io');
-
-
-
-
 // settings de express
 app.set('port',process.env.PORT || 3000);
 
-
-
-// middlewares
-
-
 // static files
 app.use(express.static(path.join(__dirname,'public')));
-
-
-// errors
-
 
 // init Server
 const server = app.listen(app.get('port'),()=> {
     console.log('server on port '+ app.get('port'))
 });
 
-// websockets
+const SocketIO = require('socket.io');
 const io = SocketIO(server);
+
+// websockets
+
 io.on('connection',(socket)=>{
     console.log('new connection',socket.id);
     
