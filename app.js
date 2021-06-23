1f09d1cb50ac9020603ae8e324e53d68ec21431a
MiguelQuispe <fisicomiguel@gmail.com>
2021-06-23 01:25:40 -0500

    update

--- app.js
+++ app.js
@@ -1,27 +1,20 @@
+const { Socket } = require('dgram');
 const express   = require('express');
 const path  = require('path');
 const app = express();
-const bodyParser= require('body-parser');
 
-const routes = require('./routes/index');
+
+const SocketIO = require('socket.io');
+
+
+
+
 // settings de express
 app.set('port',process.env.PORT || 3000);
-app.set('views', path.join(__dirname,'views')); // define donde esta la carpeta views
-app.set('views engine','ejs');
+
+
 
 // middlewares
-app.use((req, res, next)=>{
-    console.log(`${req.url} - ${req.method}`);
-    next();
-});
-
-app.use(bodyParser.json()); // para entender la info del navegador
-app.use(bodyParser.urlencoded({extended : false}));
-
-
-
-// routes
-app.use(routes);
 
 
 // static files
@@ -30,7 +23,18 @@ app.use(express.static(path.join(__dirname,'public')));
 
 // errors
 
+
 // init Server
-app.listen(app.get('port'),()=> {
+const server = app.listen(app.get('port'),()=> {
     console.log('server on port '+ app.get('port'))
+});
+
+// websockets
+const io = SocketIO(server);
+io.on('connection',(socket)=>{
+    console.log('new connection',socket.id);
+    
+    socket.on('chat:message',(data)=>{
+        io.sockets.emit('chat:message',data);
+    });
 });
